1160a5ae5a1316d4c28f826ce9efc841a0e6b885
emersonjps <emerson20010213@gmail.com>
2024-06-03 09:08:24 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,14 @@
+const { cpfs, ips } = require("./base");
+
+// Encontrar os CPFs
+
+const regexp1 = /\./gi;
+
+console.log(cpfs.match(regexp1));
+
+//------------------------------------------------------
+
+// Encontrar os IPs
+// const regexp2 = /teste/gi;
+
+// console.log(ipValues)
